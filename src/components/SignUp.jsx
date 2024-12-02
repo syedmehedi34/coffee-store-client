2855af70b2b9974def6965184abb17580dde52e4
syedmehedi34 <syedmeehdi34@gmail.com>
2024-12-03 01:18:16 +0600

    signup page modified

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -43,10 +43,9 @@ const SignUp = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign Up now!</h1>
-        </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="text-5xl font-bold mt-5 text-center">Sign Up now!</h1>
+
           <form onSubmit={handleSignUp} className="card-body min-w-96">
             <div className="form-control">
               <label className="label">
